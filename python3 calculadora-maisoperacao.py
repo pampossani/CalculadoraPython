1332e0edb85aac34fde2b83bc92bd8637466ead9
Pamela Regina Boaventura Possani <18-02239@engelogtec.com.br>
2022-08-26 14:09:23 -0300

    adicionando mais operações

--- python3 calculadora-maisoperacao.py
+++ python3 calculadora-maisoperacao.py
@@ -1,10 +1,12 @@
 #py calculadora.py
 #!/usr/bin/env python
 # encoding: utf-8
+import os
 from os import system
-from os import sys 
+from os import sys
+while True:
 
-class Calculadora:
+        class Calculadora:
     
             print('*** Calculadora em Python ***')
             print('1 - Soma | 2 - Subtração | 3 - Divisão | 4 - Multiplicação | 5 - Sair')
@@ -17,17 +19,36 @@ class Calculadora:
                 resultado = first_number + second_number
                 print ( 'O resultado da soma é '+ str(resultado))
                 
-            if operacao == '2': 
+            elif operacao == '2': 
                 first_number = int(input('Digite o primeiro número: '))
                 second_number = int(input('Digite o segundo número: '))
                 resultado = first_number - second_number
                 print ( 'O resultado da subtração é '+ str(resultado))
-                
-   
 
-            if operacao == 5:
+                
+            elif operacao == '3': 
+                first_number = float(input('Digite o primeiro número: '))
+                second_number = float(input('Digite o segundo número: '))
+                resultado = first_number / second_number
+                print ( 'O resultado da divisão é '+ str(resultado))
+              
+              
+            elif operacao == '4': 
+                first_number = float(input('Digite o primeiro número: '))
+                second_number = float(input('Digite o segundo número: '))
+                resultado = first_number * second_number
+                print ( 'O resultado da multiplicação é '+ str(resultado))
+                os.system('clear')
+  
+
+            elif operacao == '5':
                 sys.exit()
 
+            else :
+                print ('Operação inválida!')   
+                
+        input('Pressione Enter para voltar ao menu!')
+        os.system('cls')
 
 if __name__ == '__main__':
 
