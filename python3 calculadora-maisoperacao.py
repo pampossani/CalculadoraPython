3fa8ac8076b24cb7c9fd8616b5f96d0fa7a6a893
Pamela Regina Boaventura Possani <18-02239@engelogtec.com.br>
2022-08-26 12:27:43 -0300

    criando uma calculadora

---
+++ python3 calculadora-maisoperacao.py
@@ -0,0 +1,31 @@
+#py calculadora.py
+#!/usr/bin/env python
+# encoding: utf-8
+from os import system
+from os import sys 
+
+class Calculadora:
+    
+            print('*** Calculadora em Python ***')
+            print('1 - Soma | 2 - Subtração | 3 - Divisão | 4 - Multiplicação | 5 - Sair')
+            operacao = input ('\nQual operação deseja realizar? ')
+
+
+            if operacao == '1': 
+                first_number = int(input('Digite o primeiro número: '))
+                second_number = int(input('Digite o segundo número: '))
+                resultado = first_number + second_number
+                print ( 'O resultado da soma é '+ str(resultado))
+                
+
+            if operacao == 5:
+                sys.exit()
+
+
+if __name__ == '__main__':
+
+    try:
+        Calculadora()
+
+    except Exception:
+        print (Exception)
---
+++ python3 calculadora.py
@@ -0,0 +1,8 @@
+#py calculadora.py
+print('Programa  de Calculadora - Soma')
+first_number = input('primeiro número: ')
+second_number = input('segundo número: ')
+#print(first_number + second_number) #cadeia de caractere
+print(int(first_number) + int(second_number)) #soma
+print("my message")
+
